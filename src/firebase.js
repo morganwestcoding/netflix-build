bfe28b5ffa43922db2b3262776f4a80d2e3d93a4
Morgan West <mewwes@gmail.com>
2023-07-07 21:20:58 -0700

    firebase init

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC37Ky21j6hUY6DTjZtxLyIUsOdgGl_owg",
+  authDomain: "netflix-clone-morgan.firebaseapp.com",
+  projectId: "netflix-clone-morgan",
+  storageBucket: "netflix-clone-morgan.appspot.com",
+  messagingSenderId: "1092647547868",
+  appId: "1:1092647547868:web:09c8d99877cc8b413e1519"
+};
