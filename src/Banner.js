60c7a6b507f99efc5dd62b95341415d0c972ad33
Morgan West <mewwes@gmail.com>
2023-07-21 00:50:05 -0700

    banner update

--- src/Banner.js
+++ src/Banner.js
@@ -3,9 +3,22 @@ import "./Banner.css";
 
 function Banner() {
   return (
-    <div className='banner'>
-      
+    <header className='banner' style={{
+        backgroundSize: "cover",
+        backgroundImage: `url("https://res.cloudinary.com/practicaldev/image/fetch/s--THrf5Yjw--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/n6brz4p7iq7j1mulo1nv.jpg")`,
+        backgroundPosition: "center center",
+    }}>
+<div className="banner__contents">
+    <h1 className="banner__title">Movie Name</h1>
+    <div className="banner__buttons">
+        <button className='banner_button'>Play</button>
+        <button className='banner_button'>My List</button>
     </div>
+    <h1 className="banner__description">
+        This is a test description
+    </h1>
+</div>
+    </header>
   )
 }
 
