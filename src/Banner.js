1252b1865c35c576270853aa29d750e0f45a1228
Morgan West <mewwes@gmail.com>
2023-07-22 23:53:23 -0700

    api entry

--- src/Banner.js
+++ src/Banner.js
@@ -2,6 +2,10 @@ import React from 'react';
 import "./Banner.css";
 
 function Banner() {
+
+    function truncate(string, n) {
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
   return (
     <header className='banner' style={{
         backgroundSize: "cover",
@@ -11,13 +15,14 @@ function Banner() {
 <div className="banner__contents">
     <h1 className="banner__title">Movie Name</h1>
     <div className="banner__buttons">
-        <button className='banner_button'>Play</button>
-        <button className='banner_button'>My List</button>
+        <button className='banner__button'>Play</button>
+        <button className='banner__button'>My List</button>
     </div>
     <h1 className="banner__description">
-        This is a test description
+        {truncate(`This is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test descriptionThis is a test description`, 159)}
     </h1>
 </div>
+<div className="banner--fadeBottom" />
     </header>
   )
 }
